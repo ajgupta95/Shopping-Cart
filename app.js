e52dba6e0a12fe7e0fdf16a66806be7d0f0976d7
Shivendra Jadon <shivendra@digicomm.in>
2020-02-09 16:56:51 +0530

    using templates

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const path=require('path');
 
 const app =express();
 
+app.set('view engine','ejs');
+app.set('views','views');
+
 const adminroutes=require('./routes/admin');
 const shoproutes=require('./routes/shop');
 
@@ -11,12 +14,12 @@ app.use(bodyparser.urlencoded({extended:false}));
 
 app.use(express.static(path.join(__dirname,'public')));
 
-app.use('/admin',adminroutes);
+app.use('/admin',adminroutes.routes);
 
 app.use(shoproutes);
 
 app.use((req,res)=>{
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+    res.status(404).render('404',{pageTitle:'Page not found'});
 });
 
 app.listen(3000,()=>{
--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,20 @@
 const express=require('express');
 
 const path=require('path');
+const adminData=require('./admin');
 
 const router=express.Router();
 
 router.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'../','views','shop.html'));
-});
+    const products=adminData.products;
+    res.render('shop',{
+        prods:products,
+        pageTitle:'shop',
+        path: '/',
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true
+    });
+    });
 
 module.exports= router;
